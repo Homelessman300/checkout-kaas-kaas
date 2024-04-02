4f7209e215f30cdcdbccbbbe82299bc49bcea802
Homelessman3000 <126881344+Homelessman300@users.noreply.github.com>
2024-04-02 11:44:12 +0200

    small tweks

--- src/App.jsx
+++ src/App.jsx
@@ -5,41 +5,37 @@ const App = () => {
   const [chart1Diagram, setChart1Diagram] = useState([]);
   const [chart2Diagram, setChart2Diagram] = useState([]);
 
-  const fetchPokemonDetails = async (id) => {
+  const fetchCoinDetails = async (id) => {
     try {
       const response = await fetch(`api.coincap.io/v2/assets/${id}`);
       const data = await response.json();
       return data;
     } catch (error) {
-      console.error('Error fetching Pokémon details:', error);
+      console.error('Error fetching Coin details:', error);
     }
   };
 
-  const fetchPokemonList = async () => {
+  const fetchCoinList = async () => {
     const chart1Promises = [];
     const chart2Promises = [];
 
-    // Fetching Generation 1 Pokémon (IDs 1 to 151)
     for (let i = 1; i <= 151; i++) {
-      chart1Promises.push(fetchPokemonDetails(i));
+      chart1Promises.push(fetchCoinDetails(i));
     }
 
-    // Fetching Generation 2 Pokémon (IDs 152 to 251)
     for (let i = 152; i <= 251; i++) {
-      chart2Promises.push(fetchPokemonDetails(i));
+      chart2Promises.push(fetchCoinDetails(i));
     }
 
-    // Waiting for all promises to resolve
     const gen1PokemonData = await Promise.all(chart1Promises);
     const gen2PokemonData = await Promise.all(chart2Promises);
 
-    // Updating state with Pokémon lists
     setChart1Diagram(gen1PokemonData);
     setChart2Diagram(gen2PokemonData);
   };
 
   useEffect(() => {
-    fetchPokemonList();
+    fetchCoinList();
   }, []);
 
   return (
